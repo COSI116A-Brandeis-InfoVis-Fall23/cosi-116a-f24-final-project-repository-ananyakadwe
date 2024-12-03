97a527807ebacd06ad0e66e545cb77fab38cc582
Ananya Kadwe <ananyakadwe@ananyas-air.dyn.brandeis.edu>
2024-12-02 22:33:56 -0500

    add dark mode

--- js/visualization.js
+++ js/visualization.js
@@ -1,4 +1,12 @@
 // Immediately Invoked Function Expression to limit access to variables
 ((() => {
-  
+  // Add event listener for the dark mode toggle
+document.getElementById("dark-mode-toggle").addEventListener("change", (event) => {
+  if (event.target.checked) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+});
+
 })());
